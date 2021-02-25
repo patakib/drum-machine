549f49c8c49e4452446df5f9f89db5efe8aa525f
patakib <patakibalintka@gmail.com>
2021-02-25 23:05:49 +0100

    creating jsx structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div id="drum-machine">
+        <div id="display">
+          <div id="drum-pad-container">
+            <div class="drum-pad" id="">Q</div>
+            <div class="drum-pad" id="">W</div>
+            <div class="drum-pad" id="">E</div>
+            <div class="drum-pad" id="">A</div>
+            <div class="drum-pad" id="">S</div>
+            <div class="drum-pad" id="">D</div>
+            <div class="drum-pad" id="">Z</div>
+            <div class="drum-pad" id="">X</div>
+            <div class="drum-pad" id="">C</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
